d2b0050d3e3a27473f05e1561351ef1ffd17949b
Miguel Ramos <miguel921433@gmail.com>
2023-10-25 18:39:08 -0500

    proyecto terminado

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
+        \App\Models\Department::factory(6)->create();
+        \App\Models\Employee::factory(25)->create();
+
+
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\DepartmentController;
+use App\Http\Controllers\EmployeeController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -38,6 +41,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('departments', DepartmentController::class);
+    Route::resource('employees', EmployeeController::class);
+    Route::get('graphic',[EmployeeController::class,'EmployeeByDepartment'])->name('graphic');
+    Route::get('reports',[EmployeeController::class,'reports'])->name('reports');
+
+
+
 });
 
 require __DIR__.'/auth.php';
